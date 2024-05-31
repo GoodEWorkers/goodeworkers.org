ef5adc6c26dde614ff9fbffb5865565ff3ef8f67
Morgane Debarge <morganedebarge.05@gmail.com>
2024-05-31 09:21:18 +0200

    fix#27 do the modification for make it dynamic

--- src/components/Landing/ArticlesSection.tsx
+++ src/components/Landing/ArticlesSection.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import BigRoundedLinkBtn from '../Common/BigRoundedLinkBtn';
-import ArticleImg from '../../images/ArticleImg.svg';
 import Arrow1 from '../../images/Arrow1.svg';
 import Arrow2 from '../../images/Arrow2.svg';
 
@@ -14,29 +13,70 @@ function Articles() {
   const normalImg = (e: { currentTarget: { src: string; }; }) => {
     e.currentTarget.src = Arrow1;
   };
-  const ArticleCards = [
-    {
-      icon: ArticleImg,
-      title: 'Titre lorem ipsum dolor si amet consectetur adipiscing elit',
-      writer: 'Jean Michel Lipsum',
-      readingTime: '8min',
-      tag: ['tech', 'design'],
+  const articlesList = {
+    data: {
+      articles: [
+        {
+          title: 'A first example article',
+          tags: [
+            'marketing',
+            'engineering',
+          ],
+          slug: 'lorem-ipsum',
+          coverPicture: {
+            fileName: '9070079.jpg',
+            url: 'https://eu-central-1-shared-euc1-02.graphassets.com/clv4w1nk80b3307w2881491kq/clv92kfn4100607unhrcwafxs',
+            width: 1600,
+            height: 1067,
+          },
+          author: {
+            firstName: 'Richard',
+            slug: 'r-ichard',
+            lastName: 'Raduly',
+          },
+        },
+        {
+          title: 'A secoond exammple article',
+          tags: [
+            'marketing',
+            'engineering',
+          ],
+          slug: 'lorem-ipsum',
+          coverPicture: {
+            fileName: '9070079.jpg',
+            url: 'https://eu-central-1-shared-euc1-02.graphassets.com/clv4w1nk80b3307w2881491kq/clv92kfn4100607unhrcwafxs',
+            width: 1600,
+            height: 1067,
+          },
+          author: {
+            firstName: 'Richard',
+            slug: 'r-ichard',
+            lastName: 'Raduly',
+          },
+        },
+        {
+          title: 'A third example article',
+          tags: [
+            'marketing',
+            'engineering',
+          ],
+          slug: 'lorem-ipsum',
+          coverPicture: {
+            fileName: '9070079.jpg',
+            url: 'https://eu-central-1-shared-euc1-02.graphassets.com/clv4w1nk80b3307w2881491kq/clv92kfn4100607unhrcwafxs',
+            width: 1600,
+            height: 1067,
+          },
+          author: {
+            firstName: 'Richard',
+            slug: 'r-ichard',
+            lastName: 'Raduly',
+          },
+        },
+      ],
     },
-    {
-      icon: ArticleImg,
-      title: 'Titre lorem ipsum',
-      writer: 'Jean Michel Lipsum',
-      readingTime: '8min',
-      tag: ['tech', 'design', 'design', 'loremipsum'],
-    },
-    {
-      icon: ArticleImg,
-      title: 'Titre lorem ipsum dolor si amet consectetur',
-      writer: 'Jean Michel Lipsum',
-      readingTime: '8min',
-      tag: ['tech', 'design'],
-    },
-  ];
+  };
+
   return (
     <section className="bg-black-light text-white  relative -mt-12 mb-12 px-8 pt-12 lg:pt-32 pb-6 md:pb-16 lg:pb-28 rounded-xl sm:rounded-2xl lg:rounded-3xl lg:justify-between">
       <div className="flex justify-between items-center px-10 pb-16 max-md:px-0">
@@ -59,13 +99,13 @@ function Articles() {
         />
       </div>
 
-      <div className="flex w-10/12 m-auto max-xl:flex-col max-xl:items-center max-xl:w-1/2 ">
-        {ArticleCards.map((article) => (
-          <Link to="/article" key={article.title} className="w-4/5 my-4 max-xl:w-full">
+      <div className="flex w-11/12 m-auto  max-xl:items-center max-xl:w-1/2 snap-mandatory overflow-x-scroll md:overflow-hidden ">
+        {articlesList.data.articles.map((article) => (
+          <Link to="/article" key={article.title} className="w-4/5 m-4 max-xl:w-full snap-center">
             <div>
               <div className="flex">
-                <img src={article.icon} alt="img" />
-                <img src={Arrow1} alt="Arrow" onMouseEnter={changeImg} onMouseLeave={normalImg} className="max-sm:w-1/5 relative -top-10 right-10  md:-top-24 md:right-20 lg:right-24 lg:-top-28 xl:right-24 " />
+                <img src={article.coverPicture.url} alt={article.coverPicture.fileName} className="rounded-2xl" />
+                <img src={Arrow1} alt="Arrow" onMouseEnter={changeImg} onMouseLeave={normalImg} className="max-sm:w-1/5 relative -top-12 right-10  md:-top-24 md:right-20 lg:right-24 lg:-top-28 xl:right-24 xl:-top-44" />
               </div>
               <h2 className="font-clash font-semibold text-xl py-5 w-5/6 max-lg:w-full">
                 {article.title}
@@ -73,15 +113,18 @@ function Articles() {
               <section className="flex justify-between w-4/6 pb-8 max-lg:w-full">
                 <div>
                   <p className="text-purple">writer</p>
-                  <p>{article.writer}</p>
+                  <p>
+                    {article.author.firstName}
+                    {'  '}
+                    {article.author.lastName}
+                  </p>
                 </div>
                 <div className="">
                   <p className="text-purple">reading time</p>
-                  <p>{article.readingTime}</p>
                 </div>
               </section>
               <div className="flex  w-5/6 max-md:flex-wrap">
-                {article.tag.map((element) => (
+                {article.tags.map((element) => (
                   <p
                     key={`${article.title}-${element}`}
                     className=" rounded-2xl z-0 border-white border md:border-1 mr-2 px-2"
--- src/pages/article.tsx
+++ src/pages/article.tsx
@@ -1,47 +1,50 @@
 /* eslint-disable max-len */
 import React from 'react';
 import { Link } from 'gatsby';
+import parse from 'html-react-parser';
 import Articles from '../components/Landing/ArticlesSection';
-import ArticleImg from '../images/ArticleImg.svg';
-import ArticleImgLarge from '../images/ArticleImgLarge.svg';
+import '../styles/articlePage.css';
 import Fleche from '../assets/icons/fleche.svg';
-import Video from '../images/video.svg';
 import Reseaux from '../assets/icons/reseaux.svg';
-
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
 function ArticlePage() {
-  const videos = [{ icon: Video, videoName: 'video' }];
-  const articlesImg = [
-    {
-      img: ArticleImg,
-      index: 1,
+  const payload = {
+    data: {
+      article: {
+        locale: 'fr',
+        slug: 'un-premier-article-example',
+        tags: [
+          'marketing',
+          'engineering',
+        ],
+        title: 'Titre lorem ipsum dolor si amet consectetur adipiscing elit.',
+        updatedAt: '2024-04-21T05:14:12.495711+00:00',
+        author: {
+          lastName: 'Raduly',
+          firstName: 'Richard',
+          id: 'clv924m7e07x607w45fwmxfnb',
+        },
+        createdAt: '2024-04-21T05:05:19.177792+00:00',
+        content: {
+          html: "<p></p><img src=\"https://eu-central-1-shared-euc1-02.graphassets.com/clv4w1nk80b3307w2881491kq/clv928zla0k4j07un7ght6lfu\" alt=\"Logo_long_hd.png\" title=\"Logo_long_hd.png\" width=\"680\" height=\"340\" /><img src=\"https://eu-central-1-shared-euc1-02.graphassets.com/clv4w1nk80b3307w2881491kq/clv928zla0k4j07un7ght6lfu\" alt=\"Logo_long_hd.png\" title=\"Logo_long_hd.png\" width=\"680\" height=\"340\" /><p>Il est important de prendre soin du patient, d’être suivi par le patient, mais cela arrivera à un moment tel qu’il y aura beaucoup de travail et de douleur. Nibh nisl le condiment qui est empoisonné par le condiment de la vie. Les fans du jeu n&#39;ont besoin que d&#39;un clavier. Un grand vestibule bien équipé. À moins que quelqu&#39;un ne soit plus un entraîneur qu&#39;un eleifend. Comment peut-on faire ça ? A moins que l&#39;un des élus ne sache comment étudier la vie des tireurs. <strong>Il poursuit et déclenche jusqu&#39;à</strong> détester les enf<a target='_blank' title=\"https://google.com\" href=\"https://google.com\">ants pour le reste de sa vie. Duis ultricies lac mais moche tincidunt. Tortor chez l&#39;auteur du pot maintenant cela fai</a>t peur. Car je dois boire au mauvais goût du football. Le temps de la vie que les enfants n&#39;ont pas. La vie entière a juste besoin d’une grande chaleur. Ou quel élément l&#39;oreiller n&#39;est même pas. Le plat principal de la succession du salarié a été visé par les tireurs embusqués. Mais désormais, l’élément vie sera pris en charge. Il n&#39;a besoin ni de carquois ni d&#39;ultrice pour orner le bronze. Ou un pulvinar élite seleccrice mauris pellentesque. Urne de lit vallée à deux vallées.</p><p></p><ol><li><div><em>Dans la vie, la masse laide mais un élément. L&#39;élément de l&#39;oreiller n&#39;est pas non plus aussi bon que celui du lac. </em></div></li><li><div><em>Tout le temps est attendu maintenant ou le sourire. </em></div></li><li><div><em>C&#39;est juste pur dans le soft maintenant mais c&#39;est toujours un sourire. </em></div></li></ol><p><em>Mécène ultricies mi eget mauris pharetra et ultrices neque nare. La douleur de la maladie n’est pas un sourire. Moche besoin mais urne de temps et carquois de masse. Ce n’est pas seulement qu’il est important d’être facile même pendant la période clinique. Il est très important que le client puisse répondre à ses besoins. Cette démarche a mis à néant certaines des craintes de mon Elifend.</em></p><p></p><pre><code>Il ne nécessite pas de claviériste. Boire de la bière au lit. Certains s’attendaient à boire l’arc de la vie, l’élément vie sera désormais pris en charge. On ne s’attend pas à des rires à l’heure du basket. J&#39;ai lu l&#39;ultris eros dans le cours. Tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nola. Et il s’attendait à la faim et au besoin horrible. Diam volutpat commodore mais la nécessité du besoin du phasellus fringial. Il veut que les flèches soient aussi pures qu&#39;un oreiller. Qui peut mettre du chocolat dans sa bouche ?</code></pre><p></p>",
+          markdown: "\n\n![Logo_long_hd.png](https://eu-central-1-shared-euc1-02.graphassets.com/clv4w1nk80b3307w2881491kq/clv928zla0k4j07un7ght6lfu \"Logo_long_hd.png\")\n\nIl est important de prendre soin du patient, d’être suivi par le patient, mais cela arrivera à un moment tel qu’il y aura beaucoup de travail et de douleur. Nibh nisl le condiment qui est empoisonné par le condiment de la vie. Les fans du jeu n'ont besoin que d'un clavier. Un grand vestibule bien équipé. À moins que quelqu'un ne soit plus un entraîneur qu'un eleifend. Comment peut-on faire ça ? A moins que l'un des élus ne sache comment étudier la vie des tireurs. **Il poursuit et déclenche jusqu'à** détester les enf[ants pour le reste de sa vie. Duis ultricies lac mais moche tincidunt. Tortor chez l'auteur du pot maintenant cela fai](https://google.com \"https://google.com\")t peur. Car je dois boire au mauvais goût du football. Le temps de la vie que les enfants n'ont pas. La vie entière a juste besoin d’une grande chaleur. Ou quel élément l'oreiller n'est même pas. Le plat principal de la succession du salarié a été visé par les tireurs embusqués. Mais désormais, l’élément vie sera pris en charge. Il n'a besoin ni de carquois ni d'ultrice pour orner le bronze. Ou un pulvinar élite seleccrice mauris pellentesque. Urne de lit vallée à deux vallées.\n\n\n\n1.  _Dans la vie, la masse laide mais un élément. L'élément de l'oreiller n'est pas non plus aussi bon que celui du lac._\n2.  _Tout le temps est attendu maintenant ou le sourire._\n3.  _C'est juste pur dans le soft maintenant mais c'est toujours un sourire._\n\n_Mécène ultricies mi eget mauris pharetra et ultrices neque nare. La douleur de la maladie n’est pas un sourire. Moche besoin mais urne de temps et carquois de masse. Ce n’est pas seulement qu’il est important d’être facile même pendant la période clinique. Il est très important que le client puisse répondre à ses besoins. Cette démarche a mis à néant certaines des craintes de mon Elifend._\n\n\n\n    Il ne nécessite pas de claviériste. Boire de la bière au lit. Certains s’attendaient à boire l’arc de la vie, l’élément vie sera désormais pris en charge. On ne s’attend pas à des rires à l’heure du basket. J'ai lu l'ultris eros dans le cours. Tellus rutrum tellus pellentesque eu tincidunt tortor aliquam nola. Et il s’attendait à la faim et au besoin horrible. Diam volutpat commodore mais la nécessité du besoin du phasellus fringial. Il veut que les flèches soient aussi pures qu'un oreiller. Qui peut mettre du chocolat dans sa bouche ?\n\n\n",
+        },
+        coverPicture: {
+          height: 1067,
+          fileName: '9070079.jpg',
+          url: 'https://eu-central-1-shared-euc1-02.graphassets.com/clv4w1nk80b3307w2881491kq/clv92kfn4100607unhrcwafxs',
+          width: 1600,
+        },
+      },
     },
-    {
-      img: ArticleImg,
-      index: 2,
-    },
-  ];
-  const articlesImg2 = [
-    {
-      img: ArticleImg,
-      index: 1,
-    },
-    {
-      img: ArticleImg,
-      index: 2,
-    },
-    {
-      img: ArticleImg,
-      index: 3,
-    },
-  ];
+  };
   return (
     <>
       <Navbar />
       <main>
-        <section className="text-white">
+        <section className="article text-white">
           <div className="text-white flex font-clash px-4 md:px-52 py-4 ">
             <Link
               to="/"
@@ -53,19 +56,16 @@ function ArticlePage() {
             </Link>
           </div>
           <div className="px-4 md:px-52 flex-col py-6 ">
-            <aside className="lg:text-8xl  md:text-6xl font-clash text-3xl w-full">
-              Titre lorem ipsum dolor si amet consectetur adipiscing elit.
-            </aside>
-            <div className="flex-col items-center py-12 md:flex  ">
-              <img src={ArticleImgLarge} alt="ArticleImg" className="w-full " />
+            <h1>{payload.data.article.title}</h1>
+            <div className="flex-col items-center py-12 md:flex ">
+              <img src={payload.data.article.coverPicture.url} alt="ArticleImg" className="rounded-2xl" />
               <div className="bg-black-light md:w-48 rounded-2xl md:absolute md:left-4 px-2 md:px-4 m-auto w-full">
-                <div className="flex py-4">
-                  <div className=" rounded-xl z-0 border-white border md:border-1 mr-2 px-2">
-                    tech
-                  </div>
-                  <div className=" rounded-xl z-0 border-white border md:border-1 mr-2 px-2">
-                    design
-                  </div>
+                <div className="flex py-4 flex-wrap">
+                  {payload.data.article.tags.map((tag) => (
+                    <div className=" rounded-xl border-white border md:border-1 mr-2 px-2 m-2">
+                      {tag}
+                    </div>
+                  ))}
                 </div>
                 <section className=" w-4/6  pb-8 max-lg:w-full">
                   <div className="border-b-2 mb-2">
@@ -83,110 +83,8 @@ function ArticlePage() {
                 </section>
               </div>
             </div>
-            <h2 className="text-xl py-12 font-semibold max-md:text-lg">
-              Quis autem vel eum iure reprehenderit qui in ea voluptate velit
-              esse quam nihil molestiae consequatur, vel illum qui dolorem eum
-              fugiat quo voluptas nulla pariatur?
-            </h2>
-            <p>
-              At vero eos et accusamus et iusto odio dignissimos ducimus qui
-              blanditiis praesentium voluptatum deleniti atque corrupti quos
-              dolores et quas molestias excepturi sint occaecati cupiditate non
-              provident, similique sunt in culpa qui officia deserunt mollitia
-              animi, id est laborum et dolorum fuga. Et harum quidem rerum
-              facilis est et expedita distinctio. Nam libero tempore, cum soluta
-              nobis est eligendi optio cumque nihil impedit quo minus id quod
-              maxime placeat facere possimus, omnis voluptas assumenda est,
-              omnis dolor repellendus. Temporibus autem quibusdam et aut
-              officiis debitis aut rerum necessitatibus
-              {' '}
-              <span className="text-yellow underline">saepe eveniet</span>
-              {' '}
-              ut et
-              voluptates repudiandae sint et molestiae non recusandae. Itaque
-              earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
-              voluptatibus maiores alias consequatur aut perferendis doloribus
-              asperiores repellat
-            </p>
-            {videos.map((video) => (
-              <img
-                key={video.videoName}
-                src={video.icon}
-                alt="Video"
-                className="w-full m-auto py-12"
-              />
-            ))}
-            <p className="text-lg font-semibold">
-              1. At vero eos et accusamus et iusto odio dignissimos
-            </p>
-            <span className="pt-12">
-              Ducimus qui blanditiis praesentium voluptatum deleniti atque
-              corrupti quos dolores et quas molestias excepturi sint occaecati
-              cupiditate non provident, similique sunt in culpa qui officia
-              deserunt mollitia animi, id est laborum et dolorum fuga. Et harum
-              quidem rerum facilis est et expedita distinctio. Nam libero
-              tempore, cum soluta nobis est eligendi optio
-              {' '}
-              {' '}
-            </span>
-            <span className="text-yellow underline">
-              cumque nihil impedit quo minus
-            </span>
-            <span>
-              {' '}
-              {' '}
-              id quod maxime placeat facere possimus, omnis voluptas
-              assumenda est, omnis dolor repellendus. Temporibus autem quibusdam
-              et aut officiis debitis aut rerum necessitatibus saepe eveniet ut
-              et voluptates repudiandae sint et molestiae non recusandae. Itaque
-              earum rerum hic tenetur a sapiente delectus, ut aut reiciendis
-              voluptatibus maiores alias consequatur aut perferendis doloribus
-              asperiores repellat
-            </span>
-            <p className="italic p-12 max-md:p-6">
-              Quis autem vel eum iure reprehenderit qui in ea voluptate velit
-              esse quam nihil molestiae consequatur, vel illum qui dolorem eum
-              fugiat quo voluptas nulla pariatur? that they cannot foresee the
-              pain and trouble that are bound to ensue; and equal blame belongs
-              to those who fail in their duty through weakness.
-            </p>
-            <div className="flex justify-around max-sm:flex-col">
-              {articlesImg.map((element) => (
-                <img
-                  key={element.index}
-                  src={element.img}
-                  alt="ArticleImg"
-                  className=" mb-2 "
-                />
-              ))}
-            </div>
-            <p className="py-12 font-semibold">
-              2. At vero eos et accusamus et iusto odio dignissimos
-            </p>
-            <p>
-              Ducimus qui blanditiis praesentium voluptatum deleniti atque
-              corrupti quos dolores et quas molestias excepturi sint occaecati
-              cupiditate non provident, similique sunt in culpa qui officia
-              deserunt mollitia animi, id est laborum et dolorum fuga. Et harum
-              quidem rerum facilis est et expedita distinctio.
-              <br />
-              _ Nam libero tempore, cum soluta nobis est eligendi uod
-              <br />
-              _ Maxime placeat facere possimus, omnis voluptas assumenda est,
-              omnis dolor repellendus
-              <br />
-              _ Temporibus autem quibusdam et aut officiis debitis aut
-              rerum
-            </p>
-            <div className="flex py-12 justify-around pb-24 max-sm:flex-col ">
-              {articlesImg2.map((element) => (
-                <img
-                  key={element.index}
-                  src={element.img}
-                  alt="ArticleImg"
-                  className="max-md:py-4"
-                />
-              ))}
+            <div className="articleBody">
+              {parse(payload.data.article.content.html)}
             </div>
           </div>
         </section>
